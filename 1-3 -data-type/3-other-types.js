b076c7dee4bbe48c91fedb5f5b3352278970d65e
rhehdgus8831 <rhehdgus8831@naver.com>
2025-05-27 11:59:00 +0900

    변수와 데이터 타입 - 문자열,논리,기타 타입,형변환

---
+++ 1-3 -data-type/3-other-types.js
@@ -0,0 +1,29 @@
+
+// 논리(boolean) 타입  ("가 들어가면 string)
+let flag = false ;
+console.log(typeof flag);
+
+let result = 10 > 5;
+console.log(typeof  result);
+console.log(result);
+
+let fruit ='사과';
+let apple = fruit;
+console.log(apple);
+
+let True =true ;
+let False = false;
+
+let finalResult = True;
+let finalResult2 = False;
+
+// null 타입 - 부존재 (명시적인 없음) 0이라고 하면 있는거임 null이 진짜 없음
+let money = null;
+console.log(money)
+// undefined - 변수가 아직 사용준비가 안됐다.
+let yourName;
+console.log(yourName)
+
+
+
+
