dcfa60d1af49dd85ee11b9fd5de02ac46cc36a41
rhehdgus8831 <rhehdgus8831@naver.com>
2025-05-28 12:49:32 +0900

    연산자 - 반복문

---
+++ 1-4 operators/pratice-1.js
@@ -0,0 +1,10 @@
+
+let name = prompt('당신의 이름은?');
+
+let age = prompt(`당신의 나이는?`);
+
+// 출생년도 구하기
+let birthYear = 2025 - +age + 1
+
+alert(`${name}님은 ${birthYear}년생이시군요?`);
+alert(`${birthYear}`년생은 xxx띠 입니다.)
---
+++ 1-5-conditional-statement/practice1.js
@@ -0,0 +1,37 @@
+
+
+
+let score = prompt(`점수를 입력하세요`)
+
+if(+score>=90){
+    alert(`A`)
+}else if(+score>=80){
+    alert(`B`)
+}else if(+score>=70){
+    alert(`C`)
+}else if(+score>=60){
+    alert(`D`)
+}else (+score > 50)
+    alert(`F`)
+
+
+
+let score = +prompt('점수를 입력하세요');
+
+let grade;
+if (score >= 90) {
+    grade = 'A';
+} else if (score >= 80) {
+    grade = 'B';
+} else if (score >= 70) {
+    grade = 'C';
+} else if (score >= 60) {
+    grade = 'D';
+} else {
+    grade = 'F';
+}
+
+alert(grade); // 예상 출력: A
+
+
+// 90이상 a 80이상 b 70이상 c 60이상 d 나머지 f
