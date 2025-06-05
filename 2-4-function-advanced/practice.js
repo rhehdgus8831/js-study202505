3c6b97527d9e486c0d40b5e62e443a7ae225de45
rhehdgus8831 <rhehdgus8831@naver.com>
2025-06-05 17:37:33 +0900

    함수 고급 -> 콜백 예제

---
+++ 2-4-function-advanced/practice.js
@@ -0,0 +1,130 @@
+const userList = [
+    {
+        account: 'abc1234',
+        userName: '대길이',
+        job: '추노',
+        address: '서울',
+        hobbys: ['수영', '축구', '테니스'],
+        salary: 5400000,
+        age: 35,
+    },
+    {
+        account: 'banana',
+        userName: '빠나나',
+        job: '과일',
+        address: '서울',
+        hobbys: ['푸드파이팅', '테니스'],
+        salary: 9700000,
+        age: 18,
+    },
+    {
+        account: 'park1234',
+        userName: '주차왕',
+        job: '발렛파킹',
+        address: '경기',
+        hobbys: ['족구', '축구', '테니스', '영화감상'],
+        salary: 3900000,
+        age: 56,
+    },
+    {
+        account: 'fire',
+        userName: '불꽃남자카리스마',
+        job: '게이머',
+        address: '서울',
+        hobbys: ['독서', '테니스'],
+        salary: 1900000,
+        age: 42,
+    },
+];
+
+// userList의 모든 요소가 조건을 만족하면 true,
+// 하나라도 만족하지 않으면 false를 반환하는 every()함수를 구현하세요.
+
+function every(callback) {
+    for (const user of userList) {
+        if (!callback(user)) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log('================================')
+// 모든 사용자가 서울에 사는가?
+const result1 = every(user => user.address === '서울');  // false
+console.log(result1);
+// 모든 사용자의 취미가 1개 이상인가?
+const result2 = every(user => user.hobbys.length > 0);  // true
+console.log(result2);
+
+
+// userList 중 하나라도 조건을 만족하면 true,
+// 모두 만족하지 않으면 false를 반환하는 some()함수를 구현하세요.
+
+function some(callback) {
+    for (const user of userList) {
+        if (callback(user)) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log('================================')
+// 이름에 '왕'이 포함된 사람이 있는가?
+const result3 = some(user => user.userName.includes('왕')); // true
+console.log(result3);
+// 경기도에 사는 사용자가 있는가?
+const result4 = some(user => user.address === '경기'); // true
+console.log(result4);
+
+// 조건을 만족하는 사용자가 하나도 없으면 true, 있으면 false를 반환하는 none() 함수를 구현하세요.
+
+function none(callback) {
+    for (const user of userList) {
+        if (callback(user)) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log('================================')
+// '게임'이라는 취미를 가진 사용자가 없는가?
+const result5 = none(user => user.hobbys.includes('게임')); // true
+console.log(result5);
+// 서울에 사는 사용자가 아무도 없는가?
+const result6 = none(user => user.address === '서울'); // false
+console.log(result6);
+
+// 고차 함수들을 하나의 custom() 함수로 묶어 확장 가능한 형태로 만들어보세요.
+
+function custom(userArray, callback, type) {
+    switch (type) {
+        case 'filter':
+            return userArray.filter(callback);
+        case 'map':
+            return userArray.map(callback);
+        case 'every':
+            return userArray.every(callback);
+        case 'some':
+            return userArray.some(callback);
+        default:
+            return null;
+    }
+}
+// type === 'filter' → 조건 만족하는 배열
+// type === 'map' → 특정 값 추출
+// type === 'every' → 모두 조건 만족?
+// type === 'some' → 하나라도 만족?
+
+
+console.log('================================')
+const result7 = custom(userList, user => user.hobbys.length === 2, 'filter');
+console.log(result7);
+console.log('================================')
+const result8 = custom(userList, user => user.job, 'map');
+console.log(result8);
+console.log('================================')
+const result9 = custom(userList, user => user.address === '서울', 'every');
+console.log(result9);
+console.log('================================')
+const result10 = custom(userList, user => user.userName.includes('왕'), 'some');
+console.log(result10);
