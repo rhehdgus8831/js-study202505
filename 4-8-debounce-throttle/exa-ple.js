06c8402e7397a03af23e0e2acacf6568186caa93
rhehdgus8831 <rhehdgus8831@naver.com>
2025-06-16 12:53:18 +0900

    디바운스

---
+++ 4-8-debounce-throttle/exa-ple.js
@@ -0,0 +1,15 @@
+
+function foo(callback){
+
+    return (greet) => {
+        callback(greet)
+    }
+}
+
+function sayHello(greet){
+    console.log(greet);
+}
+
+
+const bar = foo(sayHello);
+bar('안녕');
